2ccd4e712b557d68add849303821c6a0aaf6331c
Luccas Fialho <luccas.flh@gmail.com>
2023-04-26 18:26:12 -0300

    Função de postar novos videos na página

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -4,6 +4,25 @@ async function buscaVideos() {
     return conexaoConvertida
 }
 
+async function criaVideo(titulo, descricao, url, imagem) {
+    const conexao = await fetch('http://localhost:3000/videos', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    })
+
+    const conexaoConvertida = await conexao.json()
+    return conexaoConvertida
+}
+
 export const conectaApi = {
-    buscaVideos
+    buscaVideos,
+    criaVideo
 }
