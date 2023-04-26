970f37a98603bb87ad2ff7fda55f2537005f007b
Luccas Fialho <luccas.flh@gmail.com>
2023-04-26 17:42:24 -0300

    Busca da api e listagem

---
+++ js/mostrarVideos.js
@@ -0,0 +1,27 @@
+import { conectaApi } from "./conectaApi.js";
+
+const lista = document.querySelector('[data-lista]');
+
+function constroiCard(elemento) {
+    const video = document.createElement('li');
+    video.className = 'videos__item';
+    video.innerHTML = `<iframe width="100%" height="72%" src="${elemento.url}"
+    title="${elemento.titulo}" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen></iframe>
+<div class="descricao-video">
+    <img src="${elemento.imagem}" alt="logo canal alura">
+    <h3>${elemento.titulo}</h3>
+    <p>${elemento.descricao}</p>
+</div>`
+    return video;
+}
+
+async function listaVideos() {
+    const listaApi = await conectaApi.buscaVideos()
+    listaApi.forEach(elemento => {
+        lista.appendChild(constroiCard(elemento))
+    });
+}
+
+listaVideos()
